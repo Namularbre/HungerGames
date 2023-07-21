4ae125a72c35763e254c19f0e6c432ca5339ebbe
Namularbre (Thomas Sazerat) <81816911+Namularbre@users.noreply.github.com>
2023-07-21 14:01:55 +0200

    Added playerConnectionListener

---
+++ src/main/java/org/namumaterial/hungergames/listeners/PlayerConnectionListener.java
@@ -0,0 +1,18 @@
+package org.namumaterial.hungergames.listeners;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class PlayerConnectionListener implements Listener {
+    @EventHandler
+    public void onPlayerConnection(PlayerJoinEvent event) {
+        Player player = (Player) event.getPlayer();
+
+        final String message = ChatColor.GOLD + player.getDisplayName() + " volunteers as a tribute.";
+        Bukkit.getServer().broadcastMessage(message);
+    }
+}
