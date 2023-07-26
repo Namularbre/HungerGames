0395064e8ac1e486b6ed574a81d482185896d85e
Namularbre (Thomas Sazerat) <81816911+Namularbre@users.noreply.github.com>
2023-07-26 20:00:57 +0200

    Added PlayerKillListener

---
+++ src/main/java/org/namumaterial/hungergames/listeners/PlayerKillListener.java
@@ -0,0 +1,36 @@
+package org.namumaterial.hungergames.listeners;
+
+import org.bukkit.entity.*;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.namumaterial.hungergames.managers.HungerGameStateManager;
+import org.namumaterial.hungergames.managers.TributeManager;
+import org.namumaterial.hungergames.utils.Tribute;
+
+public class PlayerKillListener implements Listener {
+    @EventHandler
+    public void onPlayerKill(EntityDeathEvent event) {
+        if (gameIsLaunched()) {
+            LivingEntity killedEntity = event.getEntity();
+
+            Player killer = killedEntity.getKiller();
+
+            if (killer != null) {
+                Tribute tribute = TributeManager.getTribute(killer);
+
+                if (killedEntity instanceof Monster) {
+                    final int POPULARITY_GAIN_FOR_MONSTER = 50;
+                    tribute.addPopularity(POPULARITY_GAIN_FOR_MONSTER);
+                } else if (killedEntity instanceof Animals) {
+                    final int POPULARITY_GAIN_FOR_ANIMALS = 10;
+                    tribute.addPopularity(POPULARITY_GAIN_FOR_ANIMALS);
+                }
+            }
+        }
+    }
+
+    private static boolean gameIsLaunched() {
+        return HungerGameStateManager.currentState == HungerGameStateManager.State.STARTING || HungerGameStateManager.currentState == HungerGameStateManager.State.PLAYING;
+    }
+}
