1d11087a919fcba41234f77f411b4bf91646aea6
Namularbre (Thomas Sazerat) <81816911+Namularbre@users.noreply.github.com>
2023-07-27 00:33:23 +0200

    NoInteractionOnNotStartedListener Player can't interact with blocks

--- src/main/java/org/namumaterial/hungergames/listeners/NoInteractionOnNotStartedListener.java
+++ src/main/java/org/namumaterial/hungergames/listeners/NoInteractionOnNotStartedListener.java
@@ -32,30 +32,7 @@ public class NoInteractionOnNotStartedListener implements Listener {
         Block clickedBlock = event.getClickedBlock();
 
         if (action == Action.RIGHT_CLICK_BLOCK && clickedBlock != null) {
-            Material blockType = clickedBlock.getType();
-
-            if (blockType == Material.CHEST || blockType == Material.TRAPPED_CHEST ||
-                    blockType == Material.ENDER_CHEST || blockType == Material.FURNACE ||
-                    blockType == Material.BLAST_FURNACE || blockType == Material.SMOKER ||
-                    blockType == Material.CRAFTING_TABLE || blockType == Material.ENCHANTING_TABLE ||
-                    blockType == Material.ANVIL || blockType == Material.CHIPPED_ANVIL ||
-                    blockType == Material.DAMAGED_ANVIL || blockType == Material.BARREL ||
-                    blockType == Material.DISPENSER || blockType == Material.DROPPER ||
-                    blockType == Material.HOPPER || blockType == Material.BREWING_STAND ||
-                    blockType == Material.LEVER || blockType == Material.STONE_BUTTON ||
-                    blockType == Material.ACACIA_BUTTON || blockType == Material.BIRCH_BUTTON ||
-                    blockType == Material.DARK_OAK_BUTTON || blockType == Material.JUNGLE_BUTTON ||
-                    blockType == Material.OAK_BUTTON || blockType == Material.SPRUCE_BUTTON ||
-                    blockType == Material.REPEATER || blockType == Material.COMPARATOR ||
-                    blockType == Material.REDSTONE_WIRE || blockType == Material.TRIPWIRE ||
-                    blockType == Material.BELL || blockType == Material.LOOM ||
-                    blockType == Material.CARTOGRAPHY_TABLE || blockType == Material.FLETCHING_TABLE ||
-                    blockType == Material.GRINDSTONE || blockType == Material.SMITHING_TABLE ||
-                    blockType == Material.STONECUTTER || blockType == Material.LECTERN ||
-                    blockType == Material.CAULDRON || blockType == Material.COMPOSTER) {
-
-                event.setCancelled(true);
-            }
+            event.setCancelled(true);
         }
     }
 }
