5bbd8efa2494a9bb7aa4327ef842bc96c92925cd
Namularbre (Thomas Sazerat) <81816911+Namularbre@users.noreply.github.com>
2023-07-26 22:49:35 +0200

    Added PlayerMineOreListener

---
+++ src/main/java/org/namumaterial/hungergames/listeners/PlayerMineOreListener.java
@@ -0,0 +1,36 @@
+package org.namumaterial.hungergames.listeners;
+
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.namumaterial.hungergames.managers.TributeManager;
+import org.namumaterial.hungergames.utils.Tribute;
+
+public class PlayerMineOreListener implements Listener {
+
+    @EventHandler
+    public void onPlayerMineOre(BlockBreakEvent event) {
+        Player player = event.getPlayer();
+        Block block = event.getBlock();
+
+        if (block.getType() == Material.DIAMOND_ORE) {
+            final int DIAMOND_MINING_POPULARITY_GAIN = 250;
+
+            Tribute tribute = TributeManager.getTribute(player);
+            tribute.addPopularity(DIAMOND_MINING_POPULARITY_GAIN);
+        } else if (block.getType() == Material.IRON_ORE) {
+            final int IRON_MINING_POPULARITY_GAIN = 50;
+
+            Tribute tribute = TributeManager.getTribute(player);
+            tribute.addPopularity(IRON_MINING_POPULARITY_GAIN);
+        } else if (block.getType() == Material.COAL_ORE) {
+            final int COAL_MINING_POPULARITY_GAIN = 10;
+
+            Tribute tribute = TributeManager.getTribute(player);
+            tribute.addPopularity(COAL_MINING_POPULARITY_GAIN);
+        }
+    }
+}
