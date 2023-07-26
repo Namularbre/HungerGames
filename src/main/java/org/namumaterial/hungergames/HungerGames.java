9d5e8a15582b79a56a25fc8197846dfd2c7fedd3
Namularbre (Thomas Sazerat) <81816911+Namularbre@users.noreply.github.com>
2023-07-26 20:01:12 +0200

    HungerGames PlayerKillListener added to server

--- src/main/java/org/namumaterial/hungergames/HungerGames.java
+++ src/main/java/org/namumaterial/hungergames/HungerGames.java
@@ -38,6 +38,7 @@ public final class HungerGames extends JavaPlugin {
         getLogger().info("Initialising listeners...");
         getServer().getPluginManager().registerEvents(new PlayerCompassListener(), this);
         getServer().getPluginManager().registerEvents(new PlayerDeathListener(), this);
+        getServer().getPluginManager().registerEvents(new PlayerKillListener(), this);
         getServer().getPluginManager().registerEvents(new ShieldCraftListener(), this);
         getServer().getPluginManager().registerEvents(new PlayerConnectionListener(), this);
         getServer().getPluginManager().registerEvents(new PlayerDisconnectionListener(), this);
