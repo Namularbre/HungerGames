465331daf7bf808726c87d5d7fbe662a40f9b3dd
Namularbre (Thomas Sazerat) <81816911+Namularbre@users.noreply.github.com>
2023-07-27 17:10:45 +0200

    PlayerConnectionListener simplified the listener

--- src/main/java/org/namumaterial/hungergames/listeners/PlayerConnectionListener.java
+++ src/main/java/org/namumaterial/hungergames/listeners/PlayerConnectionListener.java
@@ -1,7 +1,5 @@
 package org.namumaterial.hungergames.listeners;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -16,8 +14,7 @@ public class PlayerConnectionListener implements Listener {
     public void onPlayerConnection(PlayerJoinEvent event) {
         Player player = event.getPlayer();
         if (HungerGameStateManager.gameIsNotStarted()) {
-            final String message = ChatColor.GOLD +  player.getDisplayName() + " is volunteers as a tribute.";
-            event.setJoinMessage(message);
+            setJoinMessage(event, player);
 
             setPlayerNotStarted(player);
         } else {
@@ -25,13 +22,9 @@ public class PlayerConnectionListener implements Listener {
         }
     }
 
-    private void changeHungerGameState() {
-        final int NUMBER_OF_PLAYER = TributeManager.getNumberOfPlayer();
-        final int MINIMAL_AMOUNT_OF_PLAYER_BEFORE_START = 4;
-
-        if (NUMBER_OF_PLAYER < MINIMAL_AMOUNT_OF_PLAYER_BEFORE_START) {
-            Bukkit.getServer().broadcastMessage(ChatColor.GOLD + "" + (MINIMAL_AMOUNT_OF_PLAYER_BEFORE_START - NUMBER_OF_PLAYER) + " player needed before start.");
-        }
+    private static void setJoinMessage(PlayerJoinEvent event, Player player) {
+        final String MESSAGE = player.getDisplayName() + " is volunteers as a tribute.";
+        event.setJoinMessage(MESSAGE);
     }
 
     private void addPlayerToTributeManager(Player player) {
@@ -42,14 +35,10 @@ public class PlayerConnectionListener implements Listener {
         PlayerRawMessageSender.sendInformationMessage("Do /kits to see the kits, and then /kit [name] to select the kit.", player);
 
         addPlayerToTributeManager(player);
-        changeHungerGameState();
     }
 
     private void setPlayerSpectator(Player player) {
         PlayerRawMessageSender.sendInformationMessage("Game is already started, but you can have a look !", player);
         player.setGameMode(GameMode.SPECTATOR);
-        player.setCanPickupItems(false);
-        player.setAllowFlight(true);
-        player.setCollidable(false);
     }
 }
