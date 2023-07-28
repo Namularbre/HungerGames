c34d7848217ab093f61eca6a96cf07403f48d9c7
Namularbre (Thomas Sazerat) <81816911+Namularbre@users.noreply.github.com>
2023-07-28 14:16:05 +0200

    HungerGamesConfiguration Changed minimum amount of player

--- src/main/java/org/namumaterial/hungergames/utils/HungerGamesConfiguration.java
+++ src/main/java/org/namumaterial/hungergames/utils/HungerGamesConfiguration.java
@@ -1,7 +1,7 @@
 package org.namumaterial.hungergames.utils;
 
 public class HungerGamesConfiguration {
-    public static final int MINIMAL_AMOUNT_OF_PLAYER = 4;
+    public static final int MINIMAL_AMOUNT_OF_PLAYER = 1;
     public static final int SECOND_BEFORE_STARTING_GAME_WHEN_ENOUGH_PLAYER = 90;
     public static final double ARENA_START_RADIUS = 1000.0;
     public static final double ARENA_END_RADIUS = 200.0;
