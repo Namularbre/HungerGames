9c1d17b082b118c5c05566e328da19ad9ebd7016
Namularbre (Thomas Sazerat) <81816911+Namularbre@users.noreply.github.com>
2023-07-28 14:19:05 +0200

    ChangeHungerGameStateToStartingTasks ticksLeft => secondsLeft

--- src/main/java/org/namumaterial/hungergames/tasks/ChangeHungerGamesStateToStartingTasks.java
+++ src/main/java/org/namumaterial/hungergames/tasks/ChangeHungerGamesStateToStartingTasks.java
@@ -7,13 +7,12 @@ import org.namumaterial.hungergames.managers.HungerGameStateManager;
 import org.namumaterial.hungergames.managers.KitManager;
 import org.namumaterial.hungergames.managers.TributeManager;
 import org.namumaterial.hungergames.utils.HungerGamesConfiguration;
-import org.namumaterial.hungergames.utils.SecondToTicksConverter;
 
 public class ChangeHungerGamesStateToStartingTasks extends BukkitRunnable {
-    private int ticksLeft;
+    private int secondsLeft;
     private boolean finished;
     public ChangeHungerGamesStateToStartingTasks() {
-        this.ticksLeft = HungerGamesConfiguration.SECOND_BEFORE_STARTING_GAME_WHEN_ENOUGH_PLAYER;
+        this.secondsLeft = HungerGamesConfiguration.SECOND_BEFORE_STARTING_GAME_WHEN_ENOUGH_PLAYER;
         this.finished = false;
     }
 
@@ -24,10 +23,10 @@ public class ChangeHungerGamesStateToStartingTasks extends BukkitRunnable {
         }
 
         if (TributeManager.getAlivePlayers().size() >= HungerGamesConfiguration.MINIMAL_AMOUNT_OF_PLAYER) {
-            if (this.ticksLeft > 0) {
-                ticksLeft--;
-                Bukkit.getServer().broadcastMessage(ChatColor.GOLD + "Enough players are connected, starting in " + ticksLeft + " seconds");
-            } else if (this.ticksLeft == 0) {
+            if (this.secondsLeft > 0) {
+                secondsLeft--;
+                Bukkit.getServer().broadcastMessage(ChatColor.GOLD + "Enough players are connected, starting in " + secondsLeft + " seconds");
+            } else if (this.secondsLeft == 0) {
                 Bukkit.getServer().broadcastMessage(ChatColor.GOLD + "May the odds be ever in your favor!");
                 KitManager.giveKitToPlayers();
                 HungerGameStateManager.setStarting();
