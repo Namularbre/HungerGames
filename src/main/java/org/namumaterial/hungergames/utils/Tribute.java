a8f1d509879bc2a4f3a3dd623155650e9f2ea1ef
Namularbre (Thomas Sazerat) <81816911+Namularbre@users.noreply.github.com>
2023-08-20 19:02:39 +0200

    Tribute added Teammates attributes

--- src/main/java/org/namumaterial/hungergames/utils/Tribute.java
+++ src/main/java/org/namumaterial/hungergames/utils/Tribute.java
@@ -3,13 +3,18 @@ package org.namumaterial.hungergames.utils;
 import org.bukkit.entity.Player;
 import org.namumaterial.hungergames.kits.Kit;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class Tribute {
     private Kit kit;
     private int popularity;
+    private Collection<Player> teammates;
 
     public Tribute(Kit kit, int popularity) {
         this.kit = kit;
         this.popularity = popularity;
+        this.teammates = new ArrayList<>();
     }
 
     public Kit getKit() {
@@ -30,5 +35,13 @@ public class Tribute {
 
     public void addPopularity(int value) {
         this.popularity += value;
+    }
+
+    public Collection<Player> getTeammates() {
+        return teammates;
+    }
+
+    public void addTeammates(Player tribute) {
+        this.teammates.add(tribute);
     }
 }
