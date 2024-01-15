2064452ca0247c4fda258840275067433173c326
Thomas SAZERAT <namulabre@gmail.com>
2024-01-15 21:59:51 +0100

    SecondToTicksConverter: Removed unused method, add constant

--- src/main/java/org/namumaterial/hungergames/utils/SecondToTicksConverter.java
+++ src/main/java/org/namumaterial/hungergames/utils/SecondToTicksConverter.java
@@ -1,10 +1,8 @@
 package org.namumaterial.hungergames.utils;
 
 public class SecondToTicksConverter {
-    public static double convert(double seconds) {
-        return seconds * 20.0;
-    }
+    private static final int TICK_IN_SECONDS = 20;
 
-    public static int convert(int seconds) { return seconds * 20; }
+    public static int convert(int seconds) { return seconds * TICK_IN_SECONDS; }
 
 }
