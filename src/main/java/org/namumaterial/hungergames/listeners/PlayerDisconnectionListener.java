37a9adb7a518ac9515fe3dc1e8e932b3629d6287
Thomas <thomassazeratdev@gmail.com>
2026-09-24 00:32:50 +0200

    fix: Correct the PlayerDisconnectionListener to end the game when the second last player disconnect

--- src/main/java/org/namumaterial/hungergames/listeners/PlayerDisconnectionListener.java
+++ src/main/java/org/namumaterial/hungergames/listeners/PlayerDisconnectionListener.java
@@ -1,11 +1,14 @@
 package org.namumaterial.hungergames.listeners;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.namumaterial.hungergames.managers.HungerGameStateManager;
+import org.namumaterial.hungergames.managers.PlayerManager;
 import org.namumaterial.hungergames.managers.TributeManager;
 
 public class PlayerDisconnectionListener implements Listener {
@@ -18,6 +21,14 @@ public class PlayerDisconnectionListener implements Listener {
         }
 
         event.setQuitMessage(ChatColor.GOLD + "Tribute " + player.getName() + " escaped the game");
+
+        final int NUMBER_OF_PLAYER_REMAINING = PlayerManager.getNumberOfAlivePlayer();
+        final int ONE_PLAYER_LEFT = 1;
+
+        if (NUMBER_OF_PLAYER_REMAINING == ONE_PLAYER_LEFT) {
+            HungerGameStateManager.setEnded();
+            Bukkit.getServer().broadcastMessage( ChatColor.GOLD + "GAME OVER");
+        }
     }
 
     private static boolean isPlaying(Player player) {
